dc6a616aaefbd82784d9a2ee618022b3402ef048
Brad Williams <Deckoz2302@gmail.com>
2022-08-02 15:49:44 -0400

    Update 20-folders.sh

--- overseerr/rootfs/etc/cont-init.d/20-folders.sh
+++ overseerr/rootfs/etc/cont-init.d/20-folders.sh
@@ -5,4 +5,10 @@ if [ ! -d /config/addons_config/overseerr ]; then
     mkdir /config/addons_config/overseerr
 fi
 
+if [ ! -d /config/.cache/yarn ]; then
+    echo "Creating /config/.cache/yarn"
+    mkdir /config/.cache/yarn
+fi
+
 chown -R overseerr:overseerr /config/addons_config/overseerr
+chown -R overseerr:overseerr /config/.cache/yarn
