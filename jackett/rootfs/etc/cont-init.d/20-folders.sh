12dcefba3e38014ebdb01d8eec6c5298879ba451
Brad Williams <Deckoz2302@gmail.com>
2022-08-02 15:57:19 -0400

    Update 20-folders.sh

--- jackett/rootfs/etc/cont-init.d/20-folders.sh
+++ jackett/rootfs/etc/cont-init.d/20-folders.sh
@@ -6,17 +6,17 @@ if [ ! -d /share/downloads ]; then
     chown -R abc:abc /share/downloads
 fi
 
-if [ -d /config/Jackett ] && [ ! -d /config/addons_config/Jackett ]; then
-    echo "Moving to new location /config/addons_config/Jackett"
-    mkdir -p /config/addons_config/Jackett
-    chown -R abc:abc /config/addons_config/Jackett
-    mv /config/Jackett/* /config/addons_config/Jackett/
+if [ -d /config/jackett ] && [ ! -d /config/addons_config/jackett ]; then
+    echo "Moving to new location /config/addons_config/jackett"
+    mkdir -p /config/addons_config/jackett
+    chown -R abc:abc /config/addons_config/jackett
+    mv /config/Jackett/* /config/addons_config/jackett/
     rm -r /config/Jackett
     rm -r /config/jackett
 fi
 
-if [ ! -d /config/addons_config/Jackett ]; then
-    echo "Creating /config/addons_config/Jackett"
-    mkdir -p /config/addons_config/Jackett
-    chown -R abc:abc /config/addons_config/Jackett
+if [ ! -d /config/addons_config/jackett ]; then
+    echo "Creating /config/addons_config/jackett"
+    mkdir -p /config/addons_config/jackett
+    chown -R abc:abc /config/addons_config/jackett
 fi
